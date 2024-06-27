6a37ccf3ca6ae8c9583e7a444701b498ac281ad5
astoisavljevic <astoisavljevic@jatheon.com>
2024-06-27 17:29:51 +0200

    TG-2 added spotless plugin

--- domain/src/main/java/rs/in/staleksit/template/goose/domain/model/geo/Country.java
+++ domain/src/main/java/rs/in/staleksit/template/goose/domain/model/geo/Country.java
@@ -1,92 +1,90 @@
 package rs.in.staleksit.template.goose.domain.model.geo;
 
+import java.time.Instant;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.Instant;
 
 @Getter
 @Setter
 public class Country {
 
-    private Long id;
+  private Long id;
 
-    //~ Auditable
-    private String createdBy;
-    private Instant createdDate;
-    private String lastModifiedBy;
-    private Instant lastModifiedDate;
+  // ~ Auditable
+  private String createdBy;
+  private Instant createdDate;
+  private String lastModifiedBy;
+  private Instant lastModifiedDate;
 
-    private String name;
+  private String name;
 
-    private String alpha2;
+  private String alpha2;
 
-    private String alpha3;
+  private String alpha3;
 
-    private boolean visible;
+  private boolean visible;
 
-    private boolean deleted;
+  private boolean deleted;
 
-    public Country() {
-        this.visible = true;
-        this.deleted = false;
-    }
+  public Country() {
+    this.visible = true;
+    this.deleted = false;
+  }
 
-    public static Country create(final String createdBy,
-                                 final String name,
-                                 final String alpha2,
-                                 final String alpha3) {
-        Country country = new Country();
-        country.createdBy = createdBy;
-        country.createdDate = Instant.now();
-        country.lastModifiedBy = createdBy;
-        country.lastModifiedDate = Instant.now();
-        country.name = name;
-        country.alpha2 = alpha2;
-        country.alpha3 = alpha3;
-        return country;
-    }
+  public static Country create(
+      final String createdBy, final String name, final String alpha2, final String alpha3) {
+    Country country = new Country();
+    country.createdBy = createdBy;
+    country.createdDate = Instant.now();
+    country.lastModifiedBy = createdBy;
+    country.lastModifiedDate = Instant.now();
+    country.name = name;
+    country.alpha2 = alpha2;
+    country.alpha3 = alpha3;
+    return country;
+  }
 
-    public static Country initCreate(final Long id,
-                                     final String createdBy,
-                                     final String name,
-                                     final String alpha2,
-                                     final String alpha3) {
-        Country country = new Country();
-        country.id = id;
-        country.createdBy = createdBy;
-        country.createdDate = Instant.now();
-        country.lastModifiedBy = createdBy;
-        country.lastModifiedDate = Instant.now();
-        country.name = name;
-        country.alpha2 = alpha2;
-        country.alpha3 = alpha3;
-        return country;
-    }
+  public static Country initCreate(
+      final Long id,
+      final String createdBy,
+      final String name,
+      final String alpha2,
+      final String alpha3) {
+    Country country = new Country();
+    country.id = id;
+    country.createdBy = createdBy;
+    country.createdDate = Instant.now();
+    country.lastModifiedBy = createdBy;
+    country.lastModifiedDate = Instant.now();
+    country.name = name;
+    country.alpha2 = alpha2;
+    country.alpha3 = alpha3;
+    return country;
+  }
 
-    public void update(final Long id,
-                       final String lastModifiedBy,
-                       final String name,
-                       final String alpha2,
-                       final String alpha3,
-                       final boolean visible,
-                       final boolean deleted) {
-        this.id = id;
-        this.lastModifiedBy = lastModifiedBy;
-        this.lastModifiedDate = Instant.now();
-        this.name = name;
-        this.alpha2 = alpha2;
-        this.alpha3 = alpha3;
-        this.visible = visible;
-        this.deleted = deleted;
-    }
+  public void update(
+      final Long id,
+      final String lastModifiedBy,
+      final String name,
+      final String alpha2,
+      final String alpha3,
+      final boolean visible,
+      final boolean deleted) {
+    this.id = id;
+    this.lastModifiedBy = lastModifiedBy;
+    this.lastModifiedDate = Instant.now();
+    this.name = name;
+    this.alpha2 = alpha2;
+    this.alpha3 = alpha3;
+    this.visible = visible;
+    this.deleted = deleted;
+  }
 
-    public void toggleVisibility() {
-        this.visible = !this.visible;
-    }
+  public void toggleVisibility() {
+    this.visible = !this.visible;
+  }
 
-    public void setDelete(final boolean aDeleted) {
-        this.deleted = aDeleted;
-    }
-
+  public void setDelete(final boolean aDeleted) {
+    this.deleted = aDeleted;
+  }
 }
